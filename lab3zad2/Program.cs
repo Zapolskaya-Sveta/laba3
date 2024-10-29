27028cd842b88f6fff4b1ede41824bfc9ccb87fb
Zapolskaya-Sveta <zapolskaaveta@gmail.com>
2024-10-29 10:29:39 +0300

    Add files via upload

---
+++ lab3zad2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int maxR = 10;
+        int numCount;
+
+        for (int i = 0; i < maxR; i++)
+        {
+            numCount = i / 2 + 1;
+
+            for (int j = 0; j < numCount; j++)
+            {
+                if (i % 2 == 0)  
+                {
+                    if (j == numCount - 1)
+                    {
+                        Console.Write(3);
+                    }
+                    else
+                    {
+                        Console.Write(2);
+                    }
+                }
+                else  
+                {
+                    if (j == numCount - 1)
+                    {
+                        Console.Write(0);
+                    }
+                    else
+                    {
+                        Console.Write(9 + j - (numCount - 2));  
+                    }
+                }
+                if (j < numCount - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
