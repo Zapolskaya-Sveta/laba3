9722e76295f35a6af2b1366b7d9986af1aecad6f
Zapolskaya-Sveta <zapolskaaveta@gmail.com>
2024-10-29 10:28:15 +0300

    Add files via upload

---
+++ lab3zad1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int num = 100;
+        while (num <= 999)
+        {
+            int n1 = num / 100;
+            int n2 = num % 10;
+
+            if (n1 == n2)
+            {
+                Console.WriteLine(num);
+            }
+
+            num++;
+        }
+    }
+}
